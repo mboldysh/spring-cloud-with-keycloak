cda17cd27ccaa0db1ab22a91f3f42aab7c4ff8ac
mboldysh <maksym.boldysh@gmail.com>
2018-09-28 01:58:17 +0300

    Fix test-service endpoint

--- test-service/src/main/java/springcloudwithkeycloak/testservice/controller/TestController.java
+++ test-service/src/main/java/springcloudwithkeycloak/testservice/controller/TestController.java
@@ -1,4 +1,4 @@
-package springcloudwithkeycloak.gateway.controller;
+package springcloudwithkeycloak.testservice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
